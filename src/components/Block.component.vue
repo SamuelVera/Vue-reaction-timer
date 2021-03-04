8776a8a7d3e7bea822516d141462b37d0747a59f
Samuel Vera <samuelvera1999@gmail.com>
2021-03-04 17:17:04 -0400

    Lifecycle hooks lesson

--- src/components/Block.component.vue
+++ src/components/Block.component.vue
@@ -1,12 +1,35 @@
 <template>
-  <div class="block">
-    CLICK ME NOW!
+  <div v-if="showBlock" class="block">
+    CLICK ME! FAST!
   </div>
 </template>
 
 <script>
 export default {
+  //Accepted props
   props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  /**After the component has been mounted in the DOM */
+  mounted() {
+    /**Start a timeout based on the random delay given as prop */
+    setTimeout(() => {
+      //Show the block to be clicked
+      this.showBlock = true;
+    }, this.delay);
+  },
+  /**When the component data has been changed and re-rendered into the DOM */
+  updated() {
+    console.log("Component updated");
+  },
+  /**When the component has been unmounted from the DOM */
+  unmounted() {
+    //Good for cleanup
+    console.log("Component unmounted");
+  },
 };
 </script>
 
